028498d83a0f71e2d64186d70f4802f3d4bd5d7e
raphael4ndrade <raphael4ndrade@gmail.com>
2017-04-12 19:11:21 -0300

    failure..

--- app-service/app.js
+++ app-service/app.js
@@ -11,6 +11,11 @@ app.use(bodyParser.json());
 //     origin : '*',
 
 // }
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.options('*', cors());
 app.get('/list', cors(), (req, res) => {
