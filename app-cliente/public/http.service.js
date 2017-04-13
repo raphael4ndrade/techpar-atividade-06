bde5ee2f837dd8507c7ea5bf6f80672d356febe3
Leonardo Silveira <sombriks@gmail.com>
2017-04-13 12:07:09 -0300

    a solução estava bem próxima. olha cada changeset pra referência.

--- app-cliente/public/http.service.js
+++ app-cliente/public/http.service.js
@@ -1,5 +1,10 @@
-angular
-    .module("webapp").service('httpService', function($http){
-        this.save = (obj) => $http.post("/save", obj);
-        this.list = () => $http.get("/list");
-    })
+// https://github.com/sombriks/roundabout/blob/master/ambbi-morador/src/httpfilters.js
+// https://github.com/sombriks/aplicativo-catolico/blob/master/app-usuario/src/components/restapi.js
+
+angular.module("webapp").service('httpService', function ($http) {
+
+  let baseURL = "http://127.0.0.1:3000";
+  
+  this.save = (obj) => $http.post(baseURL + "/save", obj);
+  this.list = () => $http.get(baseURL + "/list");
+});
